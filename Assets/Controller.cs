5b56c261f43d5e4cc6a53a55dc1f1a6b28cc95b9
João Pedro <joaopedrobeckland@gmail.com>
2020-06-23 13:05:17 -0300

    Pre Tiro

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -5,66 +5,114 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
 
-    public float speed = 6.0f;
-    private GameObject cameraFPS;
-    private Vector3 moveDirection = Vector3.zero;
-    private CharacterController controller;
-    private float rotacaoX = 0;
-    private float rotacaoY = 0.0f;
+  public float speed = 6.0f;
+  private GameObject cameraFPS;
+  private Vector3 moveDirection = Vector3.zero;
+  private CharacterController controller;
+  private float rotacaoX = 0;
+  private float rotacaoY = 0.0f;
 
-    void Start()
+  public Rigidbody projectable;
+  public float speedshooter = 100;
+
+  void Start()
+  {
+    cameraFPS = GetComponentInChildren(typeof(Camera)).transform.gameObject;
+    cameraFPS.transform.localPosition = new Vector3(0, 1, 0);
+    cameraFPS.transform.localRotation = Quaternion.identity;
+    controller = GetComponent<CharacterController>();
+
+    Cursor.visible = false;
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //apenas movimenta o jogador se ele estiver no chão
+    if (controller.isGrounded)
     {
-        cameraFPS = GetComponentInChildren(typeof(Camera)).transform.gameObject;
-        cameraFPS.transform.localPosition = new Vector3(0,1,0);
-        cameraFPS.transform.localRotation = Quaternion.identity;
-        controller = GetComponent<CharacterController>();
+      //pega a direção da face à frente da camera
+      Vector3 direcaoFrente = new Vector3(cameraFPS.transform.forward.x, 0, cameraFPS.transform.forward.z);
+      //pega a direção da face ao lado da camera
+      Vector3 direcaoLado = new Vector3(cameraFPS.transform.right.x, 0, cameraFPS.transform.right.z);
+      //normaliza os valores para o máximo de 1, para que o jogador ande sempre na mesma velocidade
+      direcaoFrente.Normalize();
+      direcaoLado.Normalize();
+
+      //pega o valor das teclas para cima (1) e para baixo  (-1)
+      direcaoFrente = direcaoFrente * Input.GetAxis("Vertical");
+      //pega o valor das teclas para direita (1) e para esquerda  (-1)
+      direcaoLado = direcaoLado * Input.GetAxis("Horizontal");
+
+      //soma a movimentação lateral com a movimentação para frente/trás
+      Vector3 direcaoFinal = direcaoFrente + direcaoLado;
+      if (direcaoFinal.sqrMagnitude > 1)
+      {
+        direcaoFinal.Normalize();
+      }
+
+      //apenas move nas direções x e z
+      moveDirection = new Vector3(direcaoFinal.x, 0, direcaoFinal.z);
+
+      //multiplica pela velocidade que foi configurada no jogador
+      moveDirection = moveDirection * speed;
+
+      //pular
+      if (Input.GetButton(("Jump")))
+      {
+        moveDirection.y = 8.0f;
+      }
+
+
 
     }
+    //faz o jogador ir pra baixo (gravidade)
+    moveDirection.y -= 20.0f * Time.deltaTime;
 
-    // Update is called once per frame
-    void Update()
+    //faz o movimento
+    controller.Move(moveDirection * Time.deltaTime);
+
+    cameraPrimeiraPessoa();
+
+
+    if (Input.GetButtonDown("Fire1"))
     {
-        //apenas movimenta o jogador se ele estiver no chão
-        if (controller.isGrounded) {
-            //pega a direção da face à frente da camera
-            Vector3 direcaoFrente = new Vector3(cameraFPS.transform.forward.x, 0, cameraFPS.transform.forward.z);
-            //pega a direção da face ao lado da camera
-            Vector3 direcaoLado = new Vector3(cameraFPS.transform.right.x, 0, cameraFPS.transform.right.z);
-            //normaliza os valores para o máximo de 1, para que o jogador ande sempre na mesma velocidade
-            direcaoFrente.Normalize();
-            direcaoLado.Normalize();
+      Rigidbody hitPlayer;
 
-            //pega o valor das teclas para cima (1) e para baixo  (-1)
-            direcaoFrente = direcaoFrente * Input.GetAxis("Vertical");
-            //pega o valor das teclas para direita (1) e para esquerda  (-1)
-            direcaoLado = direcaoLado * Input.GetAxis("Horizontal");
-
-            //soma a movimentação lateral com a movimentação para frente/trás
-            Vector3 direcaoFinal = direcaoFrente + direcaoLado;
-            if (direcaoFinal.sqrMagnitude>1) {
-                direcaoFinal.Normalize();
-            }
-            
-            //apenas move nas direções x e z
-            moveDirection = new Vector3(direcaoFinal.x, 0, direcaoFinal.z);
-
-            //multiplica pela velocidade que foi configurada no jogador
-            moveDirection = moveDirection * speed;
-            
-            //pular
-            if (Input.GetButton(("Jump"))) {
-                moveDirection.y = 8.0f;
-            }
-
-
-
-        }
-        //faz o jogador ir pra baixo (gravidade)
-        moveDirection.y -= 20.0f * Time.deltaTime;
-
-        //faz o movimento
-        controller.Move(moveDirection * Time.deltaTime);
-
-
+      hitPlayer = InstantLate(projectable, cameraFPS.transform.position, cameraFPS.transform.location) as Rigidbody;
+      hitPlayer.velocity = cameraFPS.transform.TransformDirection(Vector3.foward + speedshooter);
     }
+  }
+
+  void cameraPrimeiraPessoa()
+  {
+    rotacaoX += Input.GetAxis("Mouse X") * 10.0f;
+    rotacaoY += Input.GetAxis("Mouse Y") * 10.0f;
+
+    rotacaoX = clampAngleFPS(rotacaoX, -360, 360);
+    rotacaoY = clampAngleFPS(rotacaoX, -80, 80);
+
+    Quaternion xq = Quaternion.AngleAxis(rotacaoX, Vector3.up);
+    Quaternion yq = Quaternion.AngleAxis(rotacaoY, -Vector3.right);
+
+    Quaternion q = Quaternion.identity * xq * yq;
+
+    cameraFPS.transform.localRotation = Quaternion.Lerp(cameraFPS.transform.localRotation, q, Time.deltaTime * 10.0f);
+
+  }
+
+  float clampAngleFPS(float angulo, float min, float max)
+  {
+    if (angulo < -360)
+    {
+      angulo += 360;
+    }
+    if (angulo > 360)
+    {
+      angulo += 360;
+    }
+
+    return Mathf.Clamp(angulo, min, max);
+  }
 }
